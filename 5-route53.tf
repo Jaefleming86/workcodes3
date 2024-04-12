78a635d0e53d21cae18a2f2b36a839be6be7e839
Joshua Fleming <joshua@Joshuas-MacBook-Air.local>
2024-04-11 21:51:20 -0500

    first commit

---
+++ 5-route53.tf
@@ -0,0 +1,12 @@
+# Route 53 alias record for the CloudFront distribution
+resource "aws_route53_record" "www" {
+  zone_id = "Z010694725NAW0ECJMHOZ"
+  name    = "jaepassports.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
